52b536a48744b21a29e57269cec2e6fa9fed0636
Alpri Else <aelse@hubspot.com>
2019-10-27 00:55:36 -0400

    Fix tab nav for Android

--- src/components/TabBarLabel.js
+++ src/components/TabBarLabel.js
@@ -7,7 +7,9 @@ import Colors from '../constants/Colors'
 const TabBarLabel = ({ focused, title}) => {
   const styles = {
     fontSize: 12,
-    color: focused ? Colors.PLUM : Colors.tabIconDefault
+    color: focused ? Colors.PLUM : Colors.tabIconDefault,
+    textAlign: 'center',
+    paddingBottom: 5,
   }
   return (
     <OpenSansLightText style={styles}>{title}</OpenSansLightText>
